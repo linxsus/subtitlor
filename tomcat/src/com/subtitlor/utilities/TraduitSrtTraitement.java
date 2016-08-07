2a4d329b7c0a586ff26422f508c908e45c13acae
xavier <xavier@192.168.1.10>
2016-08-07 19:36:52 +0200

    encord un test

--- tomcat/src/com/subtitlor/utilities/TraduitSrtTraitement.java
+++ tomcat/src/com/subtitlor/utilities/TraduitSrtTraitement.java
@@ -27,7 +27,7 @@ public class TraduitSrtTraitement {
     public static final String CHEMIN_FICHIERS = "c:\\"; 
     
 	public void execut(HttpServletRequest request, HttpServletResponse response, TraduitSrtDao traduitSrtDao) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub grrrr
 		
 		ArrayList<TraduitSrt> traduitSrt=traduitSrtDao.read();
 		for (TraduitSrt ligne:traduitSrt)
